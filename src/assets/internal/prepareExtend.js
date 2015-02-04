201641c3586a674552b7373d62feb6d3bec19dad
Mihail Slavchev <mihail.slavchev@telerik.com>
2015-02-04 10:09:36 +0200

    update prepareExtends.js file

--- src/assets/internal/prepareExtend.js
+++ src/assets/internal/prepareExtend.js
@@ -7,15 +7,44 @@ var __extends_ts = function (d, b) {
 
 var __extends = function(Child, Parent) {
 	
-	if (Parent.extends)	{
-		if	(Parent.__extended) {
+	if (Parent.extend)	{
+		if	(Parent.__isPrototypeImplementationObject) {
 			throw new Error("Can not extend an already extended native object.");
 		}
 	
+		function replace(func, child) {
+			var tmp = child.prototype[func];
+			child.prototype[func] = function() 
+			{
+				var that = this.__that;
+				
+				if (that)
+				{
+					return tmp.apply(that, arguments);
+				}
+				else
+				{
+					return tmp.apply(this, arguments);
+				}
+			}
+		}
+		
 		function extend(child, parent) {
 	        
 			if (!child.__extended) {
-	        	child.__extended = parent.extends(child.prototype);
+				Log("Typescript: calling extend with child.name: " + child.name);
+	        	child.__extended = parent.extend(child.prototype);
+	        	
+	        	for (var f in child.prototype)
+				{
+	        		if (child.prototype.hasOwnProperty(f))
+					{
+						if (typeof(child.prototype[f]) === 'function' && f !== 'constructor' && f !== '__parent' && f !== '__child')
+						{
+							replace(f, child);
+						}
+					}
+				}
 	        }
 	 
 	        return child.__extended;
@@ -31,6 +60,8 @@ var __extends = function(Child, Parent) {
 			{
 				thiz.__proto__ = new Extended();
 			}
+			
+			thiz.__proto__.__that = thiz;
 		};
 		
 		Parent.apply = function(thiz, args) {
@@ -43,13 +74,15 @@ var __extends = function(Child, Parent) {
 			{
 				thiz.__proto__ = new Extended();
 			}
+			
+			thiz.__proto__.__that = thiz;
 		};
 	}
 	
 	__extends_ts(Child, Parent);
 	
 	
-	if (Parent.extends)	{
+	if (Parent.extend)	{
 		Child.__isPrototypeImplementationObject = true;
 		Child.__proto__ = Parent;
 		Child.prototype.__parent = Parent;
