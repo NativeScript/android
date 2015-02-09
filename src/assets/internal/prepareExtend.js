457d127a9d9c367356ed5551f3a03cf821cb7b9c
blagoev <blagoev@none>
2015-02-09 15:20:27 +0200

    Fixed typescript support with requirement to do 'return native(this)' in every native extend constructor

--- src/assets/internal/prepareExtend.js
+++ src/assets/internal/prepareExtend.js
@@ -5,45 +5,61 @@ var __extends_ts = function (d, b) {
     d.prototype = new __();
 };
 
+var native = function(thiz) {
+	var result = thiz.__proto__;
+
+	for (var prop in thiz) 
+	{
+		if (thiz.hasOwnProperty(prop))
+		{
+			thiz.__proto__[prop] = thiz[prop];
+			delete thiz[prop];
+		}
+	}
+	
+	//TODO: enable when harmony proxies are supported
+//	var deadEnd = function(target, name)
+//	{
+//	    throw new TypeError("Use of typescript instance is not supported when extending native objects. Did you forget ot use 'return native(this)' in your constructor ?");
+//	}
+//	
+//	var handler = {};
+//	handler.get =  deadEnd;
+//	handler.set = deadEnd;
+//	handler.has = deadEnd;
+//	handler.deleteProperty = deadEnd; 
+//	handler.apply = deadEnd;
+//	handler.construct = deadEnd;
+//	handler.getOwnPropertyDescriptor = deadEnd; 
+//	handler.defineProperty = deadEnd;
+//	handler.getPrototypeOf = deadEnd;
+//	handler.setPrototypeOf = deadEnd;
+//	handler.enumerate = deadEnd;
+//	handler.ownKeys = deadEnd;
+//	handler.preventExtensions = deadEnd; 
+//	handler.isExtensible = deadEnd;
+//	
+//	var deadProxy = new Proxy({}, handler);
+//	thiz.__proto__ = deadProxy;
+	
+	thiz.constructor = undefined;
+	thiz.__proto__ = undefined;
+	Object.freeze(thiz);
+	Object.preventExtensions(thiz)
+	return result;
+};
+
 var __extends = function(Child, Parent) {
 	
 	if (Parent.extend)	{
 		if	(Parent.__isPrototypeImplementationObject) {
 			throw new Error("Can not extend an already extended native object.");
 		}
-	
-		function replace(func, child) {
-			var tmp = child.prototype[func];
-			child.prototype[func] = function() 
-			{
-				var that = this.__that;
-				
-				if (that)
-				{
-					return tmp.apply(that, arguments);
-				}
-				else
-				{
-					return tmp.apply(this, arguments);
-				}
-			}
-		}
 		
 		function extend(child, parent) {
 	        
 			if (!child.__extended) {
 	        	child.__extended = parent.extend(child.prototype);
-	        	
-	        	for (var f in child.prototype)
-				{
-	        		if (child.prototype.hasOwnProperty(f))
-					{
-						if (typeof(child.prototype[f]) === 'function' && f !== 'constructor' && f !== '__parent' && f !== '__child')
-						{
-							replace(f, child);
-						}
-					}
-				}
 	        }
 	 
 	        return child.__extended;
@@ -59,8 +75,6 @@ var __extends = function(Child, Parent) {
 			{
 				thiz.__proto__ = new Extended();
 			}
-			
-			thiz.__proto__.__that = thiz;
 		};
 		
 		Parent.apply = function(thiz, args) {
@@ -73,8 +87,6 @@ var __extends = function(Child, Parent) {
 			{
 				thiz.__proto__ = new Extended();
 			}
-			
-			thiz.__proto__.__that = thiz;
 		};
 	}
 	
