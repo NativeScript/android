60b262eb631b15fd59230a7e8eee2953ba0e5537
blagoev <blagoev@none>
2015-04-30 11:12:30 +0300

    Do not extract assets if app was not updated

--- src/src/com/tns/AssetExtractor.java
+++ src/src/com/tns/AssetExtractor.java
@@ -1,9 +1,18 @@
 package com.tns;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.tns.internal.ExtractPolicy;
 import com.tns.internal.FileExtractor;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 public class AssetExtractor
@@ -16,7 +25,10 @@ public class AssetExtractor
 		if (extractor != null)
 		{
 			boolean success = extractor.extract(context);
-			if (Platform.IsLogEnabled) Log.d(Platform.DEFAULT_LOG_TAG, "extract returned " + success);
+			if (Platform.IsLogEnabled)
+			{
+				Log.d(Platform.DEFAULT_LOG_TAG, "extract returned " + success);
+			}
 		}
 		else if (extractPolicy.shouldExtract(context))
 		{
--- src/src/com/tns/internal/DefaultExtractPolicy.java
+++ src/src/com/tns/internal/DefaultExtractPolicy.java
@@ -1,17 +1,129 @@
 package com.tns.internal;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import com.tns.Platform;
+
 public class DefaultExtractPolicy implements ExtractPolicy
 {
+	private final static String ASSETS_THUMB_FILENAME = "assetsThumb";
+
 	public boolean shouldExtract(android.content.Context context)
 	{
-		return true;
+		String assetsThumb = generateAssetsThumb(context);
+		if (assetsThumb != null)
+		{
+			String assetsThumbFilePath = context.getFilesDir().getPath() + File.separatorChar + ASSETS_THUMB_FILENAME;
+			String oldAssetsThumb = getCachedAssetsThumb(assetsThumbFilePath);
+			if (oldAssetsThumb == null || !assetsThumb.equals(assetsThumbFilePath))
+			{
+				saveNewAssetsThumb(assetsThumb, assetsThumbFilePath);
+				return true;
+			}
+		}
+
+		return false;
 	}
+
 	public boolean forceOverwrite()
 	{
 		return true;
 	}
+
 	public FileExtractor extractor()
 	{
 		return null;
 	}
+
+	private static String generateAssetsThumb(Context context)
+	{
+		try
+		{
+			PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+			int code = packageInfo.versionCode;
+			long updateTime = packageInfo.lastUpdateTime;
+			return String.valueOf(updateTime) + "-" + String.valueOf(code);
+		}
+		catch (PackageManager.NameNotFoundException e)
+		{
+			Log.e(Platform.DEFAULT_LOG_TAG, "Error while getting current assets thumb");
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private static String getCachedAssetsThumb(String assetsThumbFilePath)
+	{
+		try
+		{
+			File cachedThumbFile = new File(assetsThumbFilePath);
+			if (cachedThumbFile.exists())
+			{
+				FileInputStream in = new FileInputStream(cachedThumbFile);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+				String cachedThumb = reader.readLine();
+				reader.close();
+				in.close();
+				return cachedThumb;
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			Log.e(Platform.DEFAULT_LOG_TAG, "Error while getting current assets thumb");
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			Log.e(Platform.DEFAULT_LOG_TAG, "Error while getting current asstes thumb");
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private void saveNewAssetsThumb(String newThumb, String assetsThumbFile)
+	{
+		File cachedThumbFile = new File(assetsThumbFile);
+		try
+		{
+			FileOutputStream out = new FileOutputStream(cachedThumbFile, false);
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+			try
+			{
+				writer.write(newThumb);
+				writer.newLine();
+				writer.flush();
+			}
+			finally
+			{
+				writer.close();
+				out.close();
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			Log.e(Platform.DEFAULT_LOG_TAG, "Error while writting current assets thumb");
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			Log.e(Platform.DEFAULT_LOG_TAG, "Error while writting current assets thumb");
+			e.printStackTrace();
+		}
+	}
+
 }
