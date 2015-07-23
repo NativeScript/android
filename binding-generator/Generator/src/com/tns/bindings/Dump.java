a0f42601235fc29b4ebc92922acaf22494327727
blagoev <lubo@blagoev.com>
2015-07-23 13:48:52 +0300

    formated code and replaced character replace loops with single replace char call

--- binding-generator/Generator/src/com/tns/bindings/Dump.java
+++ binding-generator/Generator/src/com/tns/bindings/Dump.java
@@ -85,48 +85,74 @@ public class Dump
 	    return methodDescriptorBuilder.toString();
 	}
 
-    private void getDexDescriptor(final StringBuffer buf, final Class<?> c) {
-        Class<?> d = c;
-        while (true) {
-            if (d.isPrimitive()) {
-                char car;
-                if (d == Integer.TYPE) {
-                    car = 'I';
-                } else if (d == Void.TYPE) {
-                    car = 'V';
-                } else if (d == Boolean.TYPE) {
-                    car = 'Z';
-                } else if (d == Byte.TYPE) {
-                    car = 'B';
-                } else if (d == Character.TYPE) {
-                    car = 'C';
-                } else if (d == Short.TYPE) {
-                    car = 'S';
-                } else if (d == Double.TYPE) {
-                    car = 'D';
-                } else if (d == Float.TYPE) {
-                    car = 'F';
-                } else /* if (d == Long.TYPE) */{
-                    car = 'J';
-                }
-                buf.append(car);
-                return;
-            } else if (d.isArray()) {
-                buf.append('[');
-                d = d.getComponentType();
-            } else {
-                buf.append('L');
-                String name = d.getName();
-                int len = name.length();
-                for (int i = 0; i < len; ++i) {
-                    char car = name.charAt(i);
-                    buf.append(car == '.' ? '/' : car);
-                }
-                buf.append(';');
-                return;
-            }
-        }
-    }
+	private void getDexDescriptor(final StringBuffer buf, final Class<?> c)
+	{
+		Class<?> d = c;
+		while (true)
+		{
+			if (d.isPrimitive())
+			{
+				char car;
+				if (d == Integer.TYPE)
+				{
+					car = 'I';
+				}
+				else if (d == Void.TYPE)
+				{
+					car = 'V';
+				}
+				else if (d == Boolean.TYPE)
+				{
+					car = 'Z';
+				}
+				else if (d == Byte.TYPE)
+				{
+					car = 'B';
+				}
+				else if (d == Character.TYPE)
+				{
+					car = 'C';
+				}
+				else if (d == Short.TYPE)
+				{
+					car = 'S';
+				}
+				else if (d == Double.TYPE)
+				{
+					car = 'D';
+				}
+				else if (d == Float.TYPE)
+				{
+					car = 'F';
+				}
+				else
+				/* if (d == Long.TYPE) */{
+					car = 'J';
+				}
+				buf.append(car);
+				return;
+			}
+			else if (d.isArray())
+			{
+				buf.append('[');
+				d = d.getComponentType();
+			}
+			else
+			{
+				buf.append('L');
+				String name = d.getName().replace('.', '/');
+				buf.append(name);
+//				int len = name.length();
+//				for (int i = 0; i < len; ++i)
+//				{
+//					char car = name.charAt(i);
+//					buf.append(car == '.' ? '/' : car);
+//				}
+				buf.append(';');
+				return;
+			}
+		}
+	}
     
     /**
      * Returns the descriptor corresponding to the given Java type.
@@ -149,48 +175,74 @@ public class Dump
      * @param c
      *            the class whose descriptor must be computed.
     */
-    private static void getAsmDescriptor(final StringBuffer buf, final Class<?> c) {
-        Class<?> d = c;
-        while (true) {
-            if (d.isPrimitive()) {
-                char car;
-                if (d == Integer.TYPE) {
-                    car = 'I';
-                } else if (d == Void.TYPE) {
-                    car = 'V';
-                } else if (d == Boolean.TYPE) {
-                    car = 'Z';
-                } else if (d == Byte.TYPE) {
-                    car = 'B';
-                } else if (d == Character.TYPE) {
-                    car = 'C';
-                } else if (d == Short.TYPE) {
-                    car = 'S';
-                } else if (d == Double.TYPE) {
-                    car = 'D';
-                } else if (d == Float.TYPE) {
-                    car = 'F';
-                } else /* if (d == Long.TYPE) */{
-                    car = 'J';
-                }
-                buf.append(car);
-                return;
-            } else if (d.isArray()) {
-                buf.append('[');
-                d = d.getComponentType();
-            } else {
-                buf.append('L');
-                String name = d.getName();
-                int len = name.length();
-                for (int i = 0; i < len; ++i) {
-                    char car = name.charAt(i);
-                    buf.append(car == '.' ? '/' : car);
-                }
-                buf.append(';');
-                return;
-            }
-        }
-    } 
+	private static void getAsmDescriptor(final StringBuffer buf, final Class<?> c)
+	{
+		Class<?> d = c;
+		while (true)
+		{
+			if (d.isPrimitive())
+			{
+				char car;
+				if (d == Integer.TYPE)
+				{
+					car = 'I';
+				}
+				else if (d == Void.TYPE)
+				{
+					car = 'V';
+				}
+				else if (d == Boolean.TYPE)
+				{
+					car = 'Z';
+				}
+				else if (d == Byte.TYPE)
+				{
+					car = 'B';
+				}
+				else if (d == Character.TYPE)
+				{
+					car = 'C';
+				}
+				else if (d == Short.TYPE)
+				{
+					car = 'S';
+				}
+				else if (d == Double.TYPE)
+				{
+					car = 'D';
+				}
+				else if (d == Float.TYPE)
+				{
+					car = 'F';
+				}
+				else
+				/* if (d == Long.TYPE) */{
+					car = 'J';
+				}
+				buf.append(car);
+				return;
+			}
+			else if (d.isArray())
+			{
+				buf.append('[');
+				d = d.getComponentType();
+			}
+			else
+			{
+				buf.append('L');
+				String name = d.getName().replace('.', '/');
+				buf.append(name);
+//				int len = name.length();
+//				for (int i = 0; i < len; ++i)
+//				{
+//					char car = name.charAt(i);
+//					buf.append(car == '.' ? '/' : car);
+//				}
+				buf.append(';');
+				return;
+			}
+		}
+	}
  
     /**
      * Returns the Class  signature of the Type normalized if it is a primitive 
