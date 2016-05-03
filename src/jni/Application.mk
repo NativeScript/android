e84e8e126078ef78cbbdb497e48d141497794376
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-05-03 16:47:34 +0300

    updated ndk toolchain version to 4.9

--- src/jni/Application.mk
+++ src/jni/Application.mk
@@ -3,7 +3,7 @@ APP_ABI := armeabi-v7a x86 arm64-v8a
 APP_STL := stlport_static
 #APP_STL := gnustl_static
 APP_PLATFORM := android-17
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_OPTIM := debug
 #APP_OPTIM := release
 
