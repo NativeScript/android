1e06f95a1bc91efea5405436e82f25f7211c3afe
Peter Kanev <pip3r4o@gmail.com>
2016-07-07 10:32:16 +0300

    add tests for fix

---
+++ test-app/app/src/main/java/com/tns/tests/MyDerivedPublicInterface.java
@@ -0,0 +1,5 @@
+package com.tns.tests;
+
+public interface MyDerivedPublicInterface extends MyPublicInterface {
+    String methodOfMyDerivedPublicInterface(String in);
+}
--- test-app/app/src/main/java/com/tns/tests/MyPublicInterface.java
+++ test-app/app/src/main/java/com/tns/tests/MyPublicInterface.java
@@ -2,4 +2,6 @@ package com.tns.tests;
 
 public interface MyPublicInterface {
 	public final String Name = "My Public Interface";
+
+	String methodOfMyPublicInterface(String in);
 }
