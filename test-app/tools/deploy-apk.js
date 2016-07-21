70cc74320b8613a5f426d38d0f8240a2d2a3c95b
blagoev <lubo@blagoev.com>
2016-07-21 10:34:27 +0300

    check for apk existence and print command usage

--- test-app/tools/deploy-apk.js
+++ test-app/tools/deploy-apk.js
@@ -1,13 +1,33 @@
 // This node app deploys apk file on device
 // Parameters:
 //  - Path to APK to deploy
+var fs = require('fs');
+var path = require('path');
+
+function printHelp() {
+    console.error('Usage: deploy-apk.js path-to-apk/myapp.apk <device target>');
+    console.error('                                           -e: target the single available emulator');
+    console.error('                                           -d: target the single available device');
+    console.error('                                           -s device: target a device by its identifier');
+}
 
 if (process.argv.length < 2) {
-    console.error('Expect path to apk file to install');
-    process.exit(1);
+    console.error('Invalid argument no apk file specified.');
+    printHelp();
+    process.exit(-3);
 }
 
 var apk = process.argv[2];
+if (!fs.existsSync(apk)) {
+    console.error("Apk file not found at path:" + apk)
+    var parentDir = path.dirname(apk);
+    var parentDirFiles = fs.readdirSync(parentDir);
+    console.error("Apk search directory contains" + parentDirFiles);
+    
+    console.error("Installation failed");
+    process.exit(-4);
+}
+
 var runOnDeviceOrEmulator = process.argv[3];
 
 var proc = require('child_process');
