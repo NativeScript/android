cc9e4294d79118ecd37f92a8a79d297124e19875
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-09-28 09:31:59 +0300

    added custom classes tests

--- android-static-binding-generator/project/staticbindinggenerator/src/test/java/org/nativescript/staticbindinggenerator/test/CustomClassesTest.java
+++ android-static-binding-generator/project/staticbindinggenerator/src/test/java/org/nativescript/staticbindinggenerator/test/CustomClassesTest.java
@@ -42,6 +42,7 @@ public class CustomClassesTest {
         }
     }
 
+    /// Application
     @Test
     public void assertAttachBaseContextIsNotGenerated() throws IOException {
         File bindingsFile = Utils.getBindingsFile("datarow-unexpected-application-methods.txt");
@@ -60,8 +61,27 @@ public class CustomClassesTest {
     }
 
     @Test
+    public void assertApplicationConstructorIsAlwaysGeneratedAndItDoesNotHaveInitializeBlock() throws IOException {
+        File bindingsFile = Utils.getBindingsFile("datarow-no-application-methods.txt");
+
+        runGeneratorWithFile(bindingsFile);
+
+        List<String> methodsFromGeneratedDex = getMethodsFromDex("com.tns.NativeScriptApplication.dex");
+        List<String> expectedMethods = getOverriddenMethodsFromBindings(bindingsFile);
+
+        boolean methodIsGenerated = false;
+
+        if(methodsFromGeneratedDex.contains("initInstance")) {
+            methodIsGenerated = true;
+        }
+
+        Assert.assertEquals(false, methodIsGenerated);
+        Assert.assertEquals(0, expectedMethods.size());
+    }
+
+    @Test
     public void assertAllExpectedApplicationMethodsAreGenerated() throws IOException {
-        File bindingsFile = Utils.getBindingsFile("datarow-expected-application-methods.txt");
+        File bindingsFile = Utils.getBindingsFile("datarow-all-application-methods.txt");
 
         runGeneratorWithFile(bindingsFile);
 
@@ -78,6 +98,52 @@ public class CustomClassesTest {
         Assert.assertEquals(expectedMethods.size(), currentMethodCount);
     }
 
+    @Test
+    public void assertEqualsAndHashcodeAreAlwaysImplemented() throws IOException {
+        File bindingsFile = Utils.getBindingsFile("datarow-no-application-methods.txt");
+
+        runGeneratorWithFile(bindingsFile);
+
+        List<String> methodsFromGeneratedDex = getMethodsFromDex("com.tns.NativeScriptApplication.dex");
+        List<String> expectedMethods = new ArrayList<String>();
+        expectedMethods.add("equals__super");
+        expectedMethods.add("hashCode__super");
+        expectedMethods.add("hashCode");
+        expectedMethods.add("equals");
+
+        int currentMethodCount = 0;
+        for(String expectedMethod: expectedMethods) {
+            if(methodsFromGeneratedDex.contains(expectedMethod)) {
+                currentMethodCount++;
+            }
+        }
+
+        Assert.assertEquals(expectedMethods.size(), currentMethodCount);
+    }
+
+    @Test
+    public void assertNativeScriptHashCodeProviderIsAlwaysImplemented() throws IOException {
+        File bindingsFile = Utils.getBindingsFile("datarow-no-application-methods.txt");
+
+        runGeneratorWithFile(bindingsFile);
+
+        boolean classDefExists = checkIfStringExistsInDex("com.tns.NativeScriptApplication.dex", "Lcom/tns/NativeScriptHashCodeProvider;");
+
+        Assert.assertEquals(true, classDefExists);
+    }
+
+    @Test
+    public void assertJavaScriptImplementationIsAlwaysPresentInApplication() throws IOException {
+        File bindingsFile = Utils.getBindingsFile("datarow-no-application-methods.txt");
+
+        runGeneratorWithFile(bindingsFile);
+
+        boolean classDefExists = checkIfStringExistsInDex("com.tns.NativeScriptApplication.dex", "Lcom/tns/NativeScriptHashCodeProvider;");
+
+        Assert.assertEquals(true, classDefExists);
+    }
+
+    /// ACTIVITY
     @Test
     public void assertAllExpectedActivityMethodsAreGenerated() throws IOException {
         File bindingsFile = Utils.getBindingsFile("datarow-all-activity-methods.txt");
@@ -116,12 +182,16 @@ public class CustomClassesTest {
         Assert.assertEquals(0, currentMethodCount);
     }
 
+    ////// Helper methods
     private List<String> getOverriddenMethodsFromBindings(File bindingsFile) throws FileNotFoundException {
         List<String> expectedMethods = new ArrayList<>();
         Scanner scanner = new Scanner(bindingsFile);
         while(scanner.hasNextLine()) {
             DataRow row = new DataRow(scanner.nextLine());
-            expectedMethods.addAll(Arrays.asList(row.getMethods()));
+            String[] allMethods = row.getMethods();
+            if(allMethods.length > 0 && !allMethods[0].equals("")) {
+                expectedMethods.addAll(Arrays.asList(allMethods));
+            }
         }
 
         return expectedMethods;
@@ -143,5 +213,13 @@ public class CustomClassesTest {
             methodsFromGeneratedDex.add(namesTable.get(nameIndex));
         }
         return methodsFromGeneratedDex;
+    }
+
+    private boolean checkIfStringExistsInDex(String dexName, String what) throws IOException {
+        Dex dex = new Dex(new File(outputDir, dexName));
+        if(dex.findStringIndex(what) > 0) {
+            return true;
+        }
+        return false;
     }
 }
