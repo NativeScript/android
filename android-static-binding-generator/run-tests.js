998f629d6366f58b64b7b83714ff7620adf3cea9
Peter Kanev <pip3r4o@gmail.com>
2017-10-19 18:02:32 +0300

    add asbg parser and class generation tests

---
+++ android-static-binding-generator/run-tests.js
@@ -0,0 +1,34 @@
+(function () {
+    let { spawnSync, execSync } = require('child_process');
+
+    console.log("Installing JavaScript parser test dependencies.");
+
+    execSync('npm install');
+
+    console.log("Executing JavaScript parser tests using Jasmine.");
+
+    const subprocess = spawnSync('node', ['./node_modules/jasmine/bin/jasmine.js', 'JASMINE_CONFIG_PATH=jasmine.json'], { cwd: __dirname });
+
+    let stdout = subprocess.stdout.toString();
+    let stderr = subprocess.stderr.toString();
+    let code = subprocess.status;
+    let err = subprocess.error;
+
+    if (code !== 0) {
+        console.log("Tests runner exited with a non-zero code.");
+
+        let errorString = stdout;
+
+        if (err) {
+            errorString = err;
+        }
+
+        if (stderr) {
+            errorString = stderr;
+        }
+
+        throw new Error(errorString);
+    }
+
+    console.log(stdout);
+}());
