d2689e785754fafa4132ce38b168f9aca30f9553
vhristov5555 <vasil.hristov@progress.com>
2018-04-13 19:32:18 +0300

    Execute Unit tests for API 19, 23, 27, P.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ###############################################################################################
 # Android Runtime build script for CI.
 # This file is used by the CI only and it's not meant for regular development.
@@ -23,14 +24,18 @@ if [ "$1" != 'unit_tests_only' ]; then
     echo "Building Android Runtime with paramerter packageVersion: $PACKAGE_VERSION and commit: $GIT_COMMIT"
     ./gradlew -PpackageVersion=$PACKAGE_VERSION -PgitCommitVersion=$GIT_COMMIT
     cp dist/tns-android-*.tgz dist/tns-android.tgz
+else
+    echo "Building Android Runtime with paramerter packageVersion: $PACKAGE_VERSION and commit: $GIT_COMMIT"
+    ./gradlew -PpackageVersion=$PACKAGE_VERSION -PgitCommitVersion=$GIT_COMMIT  -PskipUnoptimized -PonlyX86
+    cp dist/tns-android-*.tgz dist/tns-android.tgz
 fi
 
 
-listOfEmulators='Emulator-Api19-Default Emulator-Api23-Default Emulator-Api27-Google Emulator-ApiP-Google'
+listOfEmulators="Emulator-Api19-Default Emulator-Api23-Default Emulator-Api27-Google Emulator-ApiP-Google"
 
 for emulator in $listOfEmulators; do
     echo "Start emulator $emulator"
-    $ANDROID_HOME/tools/emulator -avd $emulator -wipe-data -gpu on &
+    $ANDROID_HOME/tools/emulator -avd ${emulator} -wipe-data -gpu on &
 
     echo "Run Android Runtime unit tests for $emulator"
     $ANDROID_HOME/platform-tools/adb devices
@@ -39,7 +44,10 @@ for emulator in $listOfEmulators; do
     ./gradlew runtest
 
     echo "Rename unit test result"
-    mv ./test-app/dist/{android_unit_test_results.xml,$emulator.xml}
+    (
+        cp ./test-app/dist
+        mv android_unit_test_results.xml $emulator.xml
+    )
 
     echo "Stopping running emulators"
     for KILLPID in `ps ax | grep 'emulator' | grep -v 'grep' | awk ' { print $1;}'`; do kill -9 $KILLPID; done
