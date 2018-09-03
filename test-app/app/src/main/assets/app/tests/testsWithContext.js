e204db36bd71926155296efa748d1c96b29db9b2
Vasil Trifonov <v.trifonov@gmail.com>
2018-09-03 15:47:25 +0300

    Get just public methods if there's an error getting all methods

--- test-app/app/src/main/assets/app/tests/testsWithContext.js
+++ test-app/app/src/main/assets/app/tests/testsWithContext.js
@@ -1,20 +1,20 @@
 exports.run = function(cntxt)
 {
 	describe("Tests with context ", function () {
-		
+
 		var context = cntxt;
 		var myCustomEquality = function(first, second) {
 			return first == second;
 		};
-		
+
 		beforeEach(function() {
 			jasmine.addCustomEqualityTester(myCustomEquality);
 		});
-		
+
 		it("TestConstructorOverrideForBuiltinType", function () {
-			
+
 			__log("TEST: TestConstructorOverrideForBuiltinType");
-			
+
 			var ctorCalled = false;
 			var isConstructor = false;
 
@@ -24,17 +24,17 @@ exports.run = function(cntxt)
 					isConstructor = arguments[arguments.length - 1];
 				}
 			});
-			
+
 			var btn = new MyButton(context);
-			
+
 			expect(ctorCalled).toEqual(true);
 			expect(isConstructor).toEqual(true);
 		});
-		
+
 		it("TestConstructorOverrideForBuiltinTypeWithInitMethod", function () {
-			
+
 			__log("TEST: TestConstructorOverrideForBuiltinTypeWithInitMethod");
-			
+
 			var initInvocationCount = 0;
 
 			var MyDatePicker = android.widget.DatePicker.extend({
@@ -42,30 +42,30 @@ exports.run = function(cntxt)
 					++initInvocationCount;
 				}
 			});
-			
+
 			var datePicker = new MyDatePicker(context);
-			
+
 			__log("datePicker=" + datePicker);
-			
+
 			var count1 = initInvocationCount;
-			
+
 			expect(count1).toBeGreaterThan(0);
-			
+
 			datePicker.init(2014, 3, 25, null);
-			
+
 			var count2 = initInvocationCount;
-			
+
 			expect(count2).toBeGreaterThan(count1);
 		});
-		
+
 		it("TestBuiltinNestedClassCreation", function () {
-			
+
 			__log("TEST: TestBuiltinNestedClassCreation");
-			
+
 			var loader = new android.content.Loader(context);
 
 			var observer = new android.content.Loader.ForceLoadContentObserver(loader);
-			
+
 			expect(observer).not.toEqual(null);
 		});
 
@@ -82,35 +82,47 @@ exports.run = function(cntxt)
 
             expect(exceptionCaught).toBe(true);
         });
-		
+
 		it("TestPublicWindowManagerImplWithoutMetadata", function () {
-			
+
 			__log("TEST: TestPublicWindowManagerImplWithoutMetadata");
-			
+
 			var windowManagerImpl = context.getSystemService(android.content.Context.WINDOW_SERVICE);
-			
+
 			var display = windowManagerImpl.getDefaultDisplay();
-			
+
 			//__log("display.isValid=" + display.isValid());
-			
+
 			var displayInfo = display.toString();
-			
+
 			expect(displayInfo.length).toBeGreaterThan(0);
 		});
-		
+
 		it("TestCanPassCharSequenceArray", function () {
-			
+
 			__log("TEST: TestCanPassCharSequenceArray");
-			
+
 			var alert = new android.app.AlertDialog.Builder(context);
-		    
+
 		    var builder = alert.setItems(["One", "Two" ], new android.content.DialogInterface.OnClickListener({
 		        onClick: function (dialog, which) {
 		            //
 		        }
 		    }));
-		    
+
 			expect(builder).not.toEqual(null);
 		});
+
+		it("TestOldAPIForGettingMethodsListForMethodsWithParametersFromMissingType", function () {
+            __log("TEST: TestOldAPIForGettingMethodsListForMethodsWithParametersFromMissingType");
+
+            var til = new android.support.design.widget.TextInputLayout(context);
+            var editText = new android.widget.EditText(context);
+            var relativeLayout = new android.widget.RelativeLayout(context);
+            var relativeLayoutParams = new android.widget.RelativeLayout.LayoutParams(android.widget.RelativeLayout.LayoutParams.MATCH_PARENT, android.widget.RelativeLayout.LayoutParams.MATCH_PARENT);
+            relativeLayout.setLayoutParams(relativeLayoutParams);
+            editText.setHint("TEST");
+            til.addView(editText);
+        });
 	});
 };
--- test-app/runtime/src/main/java/com/tns/MethodResolver.java
+++ test-app/runtime/src/main/java/com/tns/MethodResolver.java
@@ -112,19 +112,11 @@ class MethodResolver {
                 methodOverloadsForClass.put(c, finder);
             }
 
-            if(!finder.errorGettingMethods()) {
-                ArrayList<Method> matchingMethods = finder.getMatchingMethods(methodName);
-                tryFindMatches(methodName, candidates, args, argLength, matchingMethods);
-                if (candidates.size() > iterationIndex && candidates.get(iterationIndex).y == 0) {
-                    // direct matching (distance 0) found
-                    break;
-                }
-            } else {
-                Method method = finder.getMatchingMethodWithArguments(methodName, args);
-                if(method != null) {
-                    candidates.add(new Tuple<>(method, 0));
-                    break;
-                }
+            ArrayList<Method> matchingMethods = finder.getMatchingMethods(methodName);
+            tryFindMatches(methodName, candidates, args, argLength, matchingMethods);
+            if (candidates.size() > iterationIndex && candidates.get(iterationIndex).y == 0) {
+                // direct matching (distance 0) found
+                break;
             }
 
             c = c.getSuperclass();
@@ -488,33 +480,23 @@ class MethodResolver {
         private Method[] declaredMethods;
         private HashMap<String, ArrayList<Method>> matchingMethods = new HashMap<String, ArrayList<Method>>();
         private final Class<?> clazz;
-        private final boolean couldNotGetMethods;
 
         public MethodFinder(Class<?> clazz) {
             this.clazz = clazz;
-            boolean errorGettingMethods = false;
             try {
                 this.declaredMethods = clazz.getDeclaredMethods();
             } catch (NoClassDefFoundError error) {
+                // get at least the public methods as it shouldn't fail with NoClassDefFoundError
                 // it is not a good practice to catch Errors in Java, but we have the following case:
                 // when using support library > 26.0.0 and android API < 23
                 // if we try to create android.support.design.widget.TextInputLayout and call its addView method
                 // such error is thrown for android.view.ViewStructure as it is not present in older APIs
-                // but it is used as a parameter in one of the TextInputLayout methods and this cause NoClassDefFoundError
-                this.declaredMethods = new Method[]{};
-                errorGettingMethods = true;
+                // so in that case we are going to get only the public methods using getMethods, but it won't fail with NoClassDefFoundError
+                this.declaredMethods = clazz.getMethods();
             }
-            this.couldNotGetMethods = errorGettingMethods;
-        }
-
-        public boolean errorGettingMethods() {
-            return couldNotGetMethods;
         }
 
         public ArrayList<Method> getMatchingMethods(String methodName) {
-            if(this.errorGettingMethods()) {
-                return null;
-            }
             ArrayList<Method> matches = this.matchingMethods.get(methodName);
             if (matches == null) {
                 matches = new ArrayList<Method>();
@@ -535,20 +517,6 @@ class MethodResolver {
             }
 
             return matches;
-        }
-
-        public Method getMatchingMethodWithArguments(String methodName, Object[] args) {
-            // this method is not so useful as the arguments need to match the method types directly, but still it can find a method in some cases
-            Class<?>[] types = new Class<?>[args.length];
-            for (int i = 0; i < args.length; i++) {
-                types[i] = args[i].getClass();
-            }
-
-            try {
-                return this.clazz.getDeclaredMethod(methodName, types);
-            } catch (NoSuchMethodException ex) {
-                return null;
-            }
         }
     }
 }
