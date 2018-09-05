b736346cbdbbea3d507fe9e3ece76444898eb78d
Darin Dimitrov <darin.dimitrov@gmail.com>
2018-09-05 15:22:47 +0300

    Upgrade v8 to 6.9.427.19

--- test-app/runtime/src/main/cpp/v8_inspector/src/allocation.h
+++ test-app/runtime/src/main/cpp/v8_inspector/src/allocation.h
@@ -79,10 +79,10 @@ char* StrNDup(const char* str, int n);
 // and free. Used as the default policy for lists.
 class FreeStoreAllocationPolicy {
     public:
-        INLINE(void* New(size_t size)) {
+        V8_INLINE void* New(size_t size) {
             return Malloced::New(size);
         }
-        INLINE(static void Delete(void* p)) {
+        V8_INLINE static void Delete(void* p) {
             Malloced::Delete(p);
         }
 };
