ba745889faa25bb81c5d47917c26c502399f8801
Vasil Trifonov <v.trifonov@gmail.com>
2018-11-07 17:24:11 +0200

    Added a setting to wrap calls to CallJSMethod in try catch

---
+++ test-app/build-tools/static-binding-generator/src/main/java/org/nativescript/staticbindinggenerator/DefaultValues.java
@@ -0,0 +1,30 @@
+package org.nativescript.staticbindinggenerator;
+
+import org.apache.bcel.generic.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class DefaultValues {
+    static final Map<Type, String> defaultValues = new HashMap<>();
+
+    // load
+    static {
+        defaultValues.put(Type.BOOLEAN, "false");
+        defaultValues.put(Type.BYTE, "0");
+        defaultValues.put(Type.SHORT, "0");
+        defaultValues.put(Type.INT, "0");
+        defaultValues.put(Type.LONG, "0L");
+        defaultValues.put(Type.CHAR, "'\u0000'");
+        defaultValues.put(Type.FLOAT, "0.0F");
+        defaultValues.put(Type.DOUBLE, "0.0");
+    }
+
+    public static final String defaultStringValueFor(Type type) {
+        if(defaultValues.containsKey(type)) {
+            return defaultValues.get(type);
+        } else {
+            return "null";
+        }
+    }
+}
