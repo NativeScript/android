3a10e61f9bc8274117e230cbb169dd53840639f8
Vasil Trifonov <v.trifonov@gmail.com>
2019-03-19 15:59:35 +0200

    chore: use again android.app.Activity as it is support/x independent

--- test-app/build-tools/jsparser/tests/cases/decorated_double_assignment_ts/app/myCustomActivity.android.ts
+++ test-app/build-tools/jsparser/tests/cases/decorated_double_assignment_ts/app/myCustomActivity.android.ts
@@ -1,7 +1,7 @@
 import Bundle = android.os.Bundle;
 
 @JavaProxy('org.nativescript.MyCustomActivity')
-export class TestActivity extends android.support.v7.app.AppCompatActivity {
+export class TestActivity extends android.app.Activity {
     static readonly TEST1: string = "my_test";
     public onCreate(savedInstanceState?: Bundle): void {
         super.onCreate(savedInstanceState);
