8e9696bd957083ba8f6eb15fb8a492c89751a444
vhristov5555 <vasil.hristov@progress.com>
2019-04-03 18:45:45 +0300

    Add test for implementing methods for base class. Change the way we assert.

--- test-app/app/src/main/assets/app/tests/nativeInheritanceTests/extendNativeClassInJS.js
+++ test-app/app/src/main/assets/app/tests/nativeInheritanceTests/extendNativeClassInJS.js
@@ -21,37 +21,43 @@ describe("Test extending just classes in JS", function() {
 
         var nonGenericAbstractClassBase = new NonGenericAbstractClassBase();
         var nonGenericAbstractClassBaseHelper = new com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBaseHelper()
+        var value = "Not Working!"
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-            expect(e.message).toBe(expectedMessage);
+            value = e.message;
         }
+        expect(value).toBe(expectedMessage);
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            expect(e.message).toBe(noMethodExpectedMessage);
+            value = e.message;
         }
+        expect(value).toBe(noMethodExpectedMessage);
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase);
         } catch (e) {
-            expect(e.message).toBe(noMethodExpectedMessage);
+            value = e.message;
         }
+        expect(value).toBe(noMethodExpectedMessage);
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract()\"");
-        }
+            value = e.message;
+         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract()\"");
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String)\"");
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String)\"");
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String, int)\"");
+            value = e.message;
         }
-
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String, int)\"");
     });
 
     it("Extend non generic abstract Java class without implementing any interfaces in JS: Implement all supported abstract methods ", function() {
@@ -85,47 +91,47 @@ describe("Test extending just classes in JS", function() {
 
         var nonGenericAbstractClassBase = new NonGenericAbstractClassBase();
         var nonGenericAbstractClassBaseHelper = new com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBaseHelper()
+        var value = "Not Working!"
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase);
         } catch (e) {
-
-            expect(e.message).toBe(expectedMessage);
+            value = e.message;
         }
-
+        expect(value).toBe(expectedMessage);
         try {
 
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-
-            expect(e.message).toBe(expectedMessage);
+            value = e.message;
         }
+        expect(value).toBe(expectedMessage);
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            var expectedValue = expectedMessage + "4";
-            expect(e.message).toBe(expectedValue);
+            value = e.message;
         }
-
+        var expectedValue = expectedMessage + "4";
+        expect(value).toBe(expectedValue);
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase);
         } catch (e) {
-
-            expect(e.message).toBe(expectedMessage);
+            value = e.message;
         }
-
+        expect(value).toBe(expectedMessage);
         try {
 
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-
-            expect(e.message).toBe(expectedMessage);
+            value = e.message;
         }
+        expect(value).toBe(expectedMessage);
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            var expectedValue = expectedMessage + "4";
-            expect(e.message).toBe(expectedValue);
+            value = e.message;
         }
+        var expectedValue = expectedMessage + "4";
+        expect(value).toBe(expectedValue);
     });
 
     it("Extend non generic abstract Java class without implementing any interfaces in JS: Don’t implement any abstract methods ", function() {
@@ -134,48 +140,93 @@ describe("Test extending just classes in JS", function() {
 
         var nonGenericAbstractClassBase = new NonGenericAbstractClassBase();
         var nonGenericAbstractClassBaseHelper = new com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBaseHelper()
+
+        var value = "Not Working!"
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase);
         } catch (e) {
-
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract()\"")
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract()\"");
         try {
-
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract(java.lang.String)\"");
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract(java.lang.String)\"");
         try {
             nonGenericAbstractClassBaseHelper.work(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            var expectedValue = expectedMessage + "4";
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract(java.lang.String, int)\"");
+            value = e.message;
         }
-
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"void com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.workAbstract(java.lang.String, int)\"");
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract()\"");
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract()\"");
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String)\"");
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String)\"");
         try {
             nonGenericAbstractClassBaseHelper.getStringAbstract(nonGenericAbstractClassBase, expectedMessage, 4);
         } catch (e) {
-            expect(e.message).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String, int)\"");
+            value = e.message;
         }
+        expect(value).toContain("Error: java.lang.AbstractMethodError: abstract method \"java.lang.String com.tns.sgbtestclasses.inheritance.classes.NonGenericAbstractClassBase.getStringAbstract(java.lang.String, int)\"");
     });
 
 
     it("Extend non generic Java class without implementing any interfaces in JS: Override some of its method ", function() {
-        var nonGenericSimpleClassBase = com.tns.sgbtestclasses.inheritance.classes.NonGenericSimpleClassBase.extend({
+        var expectedMessage = "ErrorMessage";
+                var noMethodExpectedMessage = "No Method!"
+                var NonGenericSimpleClassBase = com.tns.sgbtestclasses.inheritance.classes.NonGenericSimpleClassBase.extend({
+                    work: function() {
+                        if (arguments.length == 2) {
+                            var ErrorMessage = noMethodExpectedMessage
+                            throw new Error(ErrorMessage);
+                        }
+                        if (arguments.length == 1) {
+                            throw new Error(arguments[0]);
+                        }
+                        if (arguments.length == 0) {
+                            var ErrorMessage = noMethodExpectedMessage
+                            throw new Error(ErrorMessage);
+                        }
+                    }
+                });
 
-        });
+                var nonGenericSimpleClassBase = new NonGenericSimpleClassBase();
+                var nonGenericSimpleClassBaseHelper = new com.tns.sgbtestclasses.inheritance.classes.NonGenericSimpleClassBaseHelper()
+                var value = "Not Working!"
+                try {
+                    nonGenericSimpleClassBaseHelper.work(nonGenericSimpleClassBase, expectedMessage);
+                } catch (e) {
+                    value = e.message;
+                }
+                expect(value).toBe(expectedMessage);
+                try {
+                    nonGenericSimpleClassBaseHelper.work(nonGenericSimpleClassBase, expectedMessage, 4);
+                } catch (e) {
+                    value = e.message;
+                }
+                expect(value).toBe(noMethodExpectedMessage);
+                try {
+                    nonGenericSimpleClassBaseHelper.work(nonGenericSimpleClassBase);
+                } catch (e) {
+                    value = e.message;
+                }
+                expect(value).toBe(noMethodExpectedMessage);
+                var value = nonGenericSimpleClassBaseHelper.getString(nonGenericSimpleClassBase);
+                expect(value).toBe("string");
+                value = nonGenericSimpleClassBaseHelper.getString(nonGenericSimpleClassBase, expectedMessage);
+                expect(value).toBe(expectedMessage);
+                value = nonGenericSimpleClassBaseHelper.getString(nonGenericSimpleClassBase, expectedMessage, 4);
+                expect(value).toBe(expectedMessage + "4");
     });
 
     it("Extend non generic Java class without implementing any interfaces in JS: Override all supported methods ", function() {
