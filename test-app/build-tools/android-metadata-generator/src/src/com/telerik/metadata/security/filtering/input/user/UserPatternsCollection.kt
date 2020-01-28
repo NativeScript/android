fba5fa52c659be8904a67c6f31aa71c016dc3736
vmutafov <vlado.mutafov@gmail.com>
2020-01-28 19:16:11 +0200

    Whitelist everything if no whitelist file is provided

--- test-app/build-tools/android-metadata-generator/src/src/com/telerik/metadata/security/filtering/input/user/UserPatternsCollection.kt
+++ test-app/build-tools/android-metadata-generator/src/src/com/telerik/metadata/security/filtering/input/user/UserPatternsCollection.kt
@@ -2,8 +2,8 @@ package com.telerik.metadata.security.filtering.input.user
 
 import com.telerik.metadata.security.filtering.input.PatternEntry
 import com.telerik.metadata.security.filtering.input.PatternsCollection
-import java.net.URI
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 object UserPatternsCollection : PatternsCollection {
@@ -16,22 +16,28 @@ object UserPatternsCollection : PatternsCollection {
 
 
     override fun populateWhitelistEntriesFromFile(filePath: String) {
-        whitelistEntries = parseFile(filePath) +
+        val path = Paths.get(filePath)
+
+        if (!Files.exists(path)) {
+            return
+        }
+
+        whitelistEntries = parseFile(path) +
                 PatternEntry("java.lang", "Object") +
                 PatternEntry("com.tns.gen*", "*")
     }
 
     override fun populateBlacklistEntriesFromFile(filePath: String) {
-        blacklistEntries = parseFile(filePath)
-    }
-
-    private fun parseFile(filePath: String): Collection<PatternEntry> {
         val path = Paths.get(filePath)
 
         if (!Files.exists(path)) {
-            return emptyList()
+            return
         }
 
+        blacklistEntries = parseFile(path)
+    }
+
+    private fun parseFile(path: Path): Collection<PatternEntry> {
         val fileLines = Files.readAllLines(path)
         val lineSplitter = UserLineSplitter()
         val commentFilter = UserLineCommentFilter()
