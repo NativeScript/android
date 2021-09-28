b452d5395d126109d4027eb3d9803bdeb7fcf7aa
triniwiz <fortune.osei@yahoo.com>
2021-09-28 07:57:12 -0400

    fix: weakref

--- test-app/runtime/src/main/cpp/Runtime.cpp
+++ test-app/runtime/src/main/cpp/Runtime.cpp
@@ -665,8 +665,6 @@ Isolate* Runtime::PrepareV8Runtime(const string& filesPath, const string& native
     }
 #endif
 
-    m_weakRef.Init(isolate, globalTemplate, m_objectManager);
-
     SimpleProfiler::Init(isolate, globalTemplate);
 
     CallbackHandlers::CreateGlobalCastFunctions(isolate, globalTemplate);
@@ -689,6 +687,7 @@ Isolate* Runtime::PrepareV8Runtime(const string& filesPath, const string& native
     global->DefineOwnProperty(context, ArgConverter::ConvertToV8String(isolate, "global"), global, readOnlyFlags);
     global->DefineOwnProperty(context, ArgConverter::ConvertToV8String(isolate, "__global"), global, readOnlyFlags);
 
+    m_weakRef.Init(context);
     // Do not set 'self' accessor to main thread JavaScript
     if (s_mainThreadInitialized) {
         global->DefineOwnProperty(context, ArgConverter::ConvertToV8String(isolate, "self"), global, readOnlyFlags);
