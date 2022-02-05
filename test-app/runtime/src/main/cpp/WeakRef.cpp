79c0d18099faed64c2dd0ba18445cd29191c595b
Osei Fortune <fortune.osei@yahoo.com>
2022-02-05 12:32:58 -0400

    fix: weakref warnings

--- test-app/runtime/src/main/cpp/WeakRef.cpp
+++ test-app/runtime/src/main/cpp/WeakRef.cpp
@@ -11,7 +11,12 @@ namespace tns {
 
         std::string source = R"(
         global.WeakRef.prototype.get = global.WeakRef.prototype.deref;
+        global.WeakRef.prototype.__hasWarnedAboutClear = false;
         global.WeakRef.prototype.clear = () => {
+            if(global.WeakRef.prototype.__hasWarnedAboutClear) {
+                return;
+            }
+            global.WeakRef.prototype.__hasWarnedAboutClear = true;
             console.warn('WeakRef.clear() is non-standard and has been deprecated. It does nothing and the call can be safely removed.');
         }
     )";
