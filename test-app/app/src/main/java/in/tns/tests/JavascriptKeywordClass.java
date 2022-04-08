63b77250f026fa8e7077dd2ee859f3c556f5cc2c
Martin Guillon <martin.guillon@akylas.fr>
2022-04-08 10:11:34 +0200

    feat: Java methods returning java.lang primitive
    converted to JS corresponding type

--- test-app/app/src/main/java/in/tns/tests/JavascriptKeywordClass.java
+++ test-app/app/src/main/java/in/tns/tests/JavascriptKeywordClass.java
@@ -5,4 +5,10 @@ public class JavascriptKeywordClass {
     {
         return 5;
     }
+    public Int getIntValue5() {return Int.valueOf(5); }
+    public Long getLongValue5() {return Long.valueOf(5); }
+    public Float getFloatValue5() {return Float.valueOf(5); }
+    public Short getShortValue5() {return Short.valueOf(5); }
+    public Double getDoubleValue5() {return Double.valueOf(5); }
+    public Boolean getBooleanValueTrue() {return true; }
 }
