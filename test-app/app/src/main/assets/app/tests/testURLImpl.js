08376abda047d7e8b79cacfa695b2533da2f254d
Osei Fortune <fortune.osei@yahoo.com>
2024-01-19 22:45:59 -0400

    chore: format tests

--- test-app/app/src/main/assets/app/tests/testURLImpl.js
+++ test-app/app/src/main/assets/app/tests/testURLImpl.js
@@ -1,31 +1,31 @@
 describe("Test URL ", function () {
 
-it("Test invalid URL parsing", function(){
-var exceptionCaught = false;
-  try {
-        const url = new URL('');
+    it("Test invalid URL parsing", function(){
+        var exceptionCaught = false;
+        try {
+            const url = new URL('');
         }catch(e){
-        exceptionCaught = true;
+            exceptionCaught = true;
         }
         expect(exceptionCaught).toBe(true);
-});
+    });
 
-it("Test valid URL parsing", function(){
-var exceptionCaught = false;
-  try {
-        const url = new URL('https://google.com');
+    it("Test valid URL parsing", function(){
+        var exceptionCaught = false;
+        try {
+            const url = new URL('https://google.com');
         }catch(e){
-        exceptionCaught = true;
+            exceptionCaught = true;
         }
         expect(exceptionCaught).toBe(false);
-});
+    });
 
 
-it("Test URL fields", function(){
-var exceptionCaught = false;
-  const url = new URL('https://google.com');
-  expect(url.protocol).toBe('https:');
-  expect(url.hostname).toBe('google.com');
-});
+    it("Test URL fields", function(){
+        var exceptionCaught = false;
+        const url = new URL('https://google.com');
+        expect(url.protocol).toBe('https:');
+        expect(url.hostname).toBe('google.com');
+    });
 
 });
